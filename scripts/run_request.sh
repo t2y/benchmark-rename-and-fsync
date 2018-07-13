98a2dde83f1dbe76bee09b80b407af389b296103
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2018-07-13 20:26:05 +0900

    fix trivial spaces issue

--- scripts/run_request.sh
+++ scripts/run_request.sh
@@ -22,9 +22,9 @@ for rate in "${RATES[@]}"; do
         echo "request rate is $rate, target is $handler"
         now=$(date +"%Y%m%d%H%M%S")
         iostat_logfile="${TMP_DIR}/iostat-vegeta-${rate}-${handler}-${now}.log"
-	    iostat -ymxt 1 /dev/sdk > ${iostat_logfile} &
+        iostat -ymxt 1 /dev/sdk > ${iostat_logfile} &
         echo "GET http://localhost:8090/$handler" | vegeta attack -duration=$DURATION -rate $rate | tee "${RESULTS_PREFIX}-${rate}-${handler}.bin" | vegeta report
-	    pkill iostat
+        pkill iostat
         echo
     done
 done
